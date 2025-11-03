0735506bfb3d4afd37e314b4b6eb66a00341ab69
rafa-ken <rafaelkenmiyamoto@gmail.com>
2025-11-03 18:30:09 -0300

    commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,7 @@
+// vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  }
 })
